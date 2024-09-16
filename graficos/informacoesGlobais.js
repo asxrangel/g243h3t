d371e2643122879645acac59902ebe92fc014d15
Marianna Mika <110108982+asxrangel@users.noreply.github.com>
2024-09-16 12:28:44 -0300

    Add files via upload

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,9 @@
+const url= "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globalThis.json";
+
+async function vizualizarInformacoesGlobais(){
+    const res = await fetch(url);
+    const dados = res.json();
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais();
